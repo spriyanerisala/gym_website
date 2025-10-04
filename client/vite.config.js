025a38517226403f9dfabcfcb90aab432cc95fec
Siva Priya Nerisala <159527078+spriyanerisala@users.noreply.github.com>
2025-10-04 14:04:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+   build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, 
+  }
 })
